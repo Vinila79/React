c512fe36ac4684eeed88e4708e34d42347e2ac76
Vinila79 <vinilapendli15@gmail.com>
2024-07-13 15:34:54 +0530

    changes5

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import ProductListing from "./Components/class-components/productListing/productListing"
+import ProductListing from "./Components/class-components/productListing/productListing";
+// import CustomSpinner from "./Components/spinner/CustomSpinner";
 
 
 
 const App =() =>{
   return(
     <ProductListing/>
+    // <CustomSpinner/>
   )
 }
 export default App;
--- src/Components/class-components/productListing/productListing.js
+++ src/Components/class-components/productListing/productListing.js
@@ -1,30 +1,121 @@
+import CustomSpinner from "../../spinner/CustomSpinner";
+
 
 
 const { Component} = require("react");
 
 class ProductListing extends Component{
+
+state={
+    ProductListing:[],
+    loading: false,
+  error:null,
+}
+
     componentDidMount() {
         console.log("componentDidMount invoked...");
         this.fetchData();
 
     }
 
-        fetchData = () => {
-            fetch("https://fakestoreapi.com/products/1")
-            .then((response) =>response.json())
-            .then((response) =>console.log(response));
-        }
+        // fetchData = () => {
+        //     this.setState({
+        //         loading: true,
+        //     })
+        //     fetch("https://fakestoreapi.com/products")
+        //     .then((response) =>response.json())
+        //     .then((response) => {
+        //         console.log(response)
+        //         this.setState({
+        //             ProductListing: response,
+        //             loading: false,
+        //         });
+        // });      
+        // };
+
+
+        // fetchData = async ()=>{
+        //    this.setState({
+        //     loading: true,
+        //    });
+        //    try{
+        //    const response1 = await fetch("https://fakestoreapi.com/products");
+        //    const finalResponse = await response1.json();
+        //    this.setState({
+        //     ProductListing: finalResponse,
+        //     loading: false,
+        //    });
+        // } catch (err) {
+        //     console.error(err);
+        //     this.setState({
+        //         error: "server is busy",
+        //     });
+        // }finally{
+        //     this.setState({
+        //         loading: false,
+        //     });
+        // }
+        // };
+
+
+        fetchData = async ()=>{
+            this.setState({
+             loading: true,
+            });
+            try{
+            const response1 = await Axios("https://fakestoreapi.com/products");
+           console.log(response1, "response1");
+
+
+           if(response1.status === 200) {
+            this.setState({
+                ProductListing:response1.data,
+                loading: false,
+            })
+           }
+
+
+
+            // this.setState({
+            //  ProductListing: finalResponse,
+            //  loading: false,
+            // });
+         } catch (err) {
+             console.error(err);
+             this.setState({
+                 error: "server is busy",
+             });
+         }finally{
+             this.setState({
+                 loading: false,
+             });
+         }
+         };
 
     render() {
         console.log("render invoked...");
         return(
             <>
-            <h1>Product Listing page</h1>
+            <h3>Product Listing page</h3>
             
-            
+            {this.state.loading ? (
+                <>
+               
+                <h3>please wait its loading...</h3>
+                <CustomSpinner/>
+                </>
+               
+                ):(
+                    <div>
+                         {this.state.ProductListing.map(eachObject=>{
+                            return<h3 key={eachObject.id}>{eachObject.title}</h3>
+                            
+                        })}
+                    </div>
+            )}    
             </>
-        )
+        );
        
     }
 }
-export default ProductListing
+export default ProductListing;
