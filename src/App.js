2ede664b8fda073306515eef255355f950bdf53e
Vinila79 <vinilapendli15@gmail.com>
2024-07-14 16:01:36 +0530

    changes2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
-import ProductListing from "./Components/class-components/productListing/productListing";
+// import ProductListing from "./Components/class-components/productListing/productListing";
+import Greeting from "./Components/greeting/greeting";
 // import CustomSpinner from "./Components/spinner/CustomSpinner";
 
 
 
 const App =() =>{
+
+  const  persons =[
+    {
+      name: "kiran",
+      dayRouting:[["woke up at 7am" , "breakfast at 9am " , "class at 12 pm "]],
+      breakTime: 10,
+    },
+    {
+      name: "ramya",
+      dayRouting:[["woke up at 7am" , "breakfast at 9am " , "class at 12 pm "]],
+      breakTime: 60,
+    },
+  ];
+
+  const names=["kiran" , "sathwika" , "ramya" , "sashi" , "mounika"];
+  
+  const greeting=["woke up at 7am" , "breakfast at 9am " , "class at 12 pm "];
+  
+  
   return(
-    <ProductListing/>
-    // <CustomSpinner/>
-  )
-}
+    <div>
+      {
+        names.map((eachName, index) =>{
+          return(
+            <Greeting name={eachName} key={index}>
+              {
+                greeting.map((eachGreeting, ind) => {
+                  return<h4 key={ind}>{eachGreeting}</h4>
+                })};
+            </Greeting>  
+          );
+        })};
+    </div>
+ 
+  );
+};
 export default App;
