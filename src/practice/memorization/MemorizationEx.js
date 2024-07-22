1946211f72d022e11094d6b3203304cf47e0456f
Vinila79 <vinilapendli15@gmail.com>
2024-07-22 20:09:35 +0530

    changes3

---
+++ src/practice/memorization/MemorizationEx.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MemorizationEx = () => {
+  return (
+    <div>MemorizationEx</div>
+  )
+}
+
+export default MemorizationEx
---
+++ src/practice/reducehook.js
@@ -0,0 +1,20 @@
+// import { useReducer } from "react";
+
+
+
+// const ReducerFunction =() =>{
+
+    
+// };
+
+// const initialstate ={
+//     state=10;
+// }
+
+
+
+// const UseReducerExample = () =>{
+//  const [current state , dispatch function ]=useReducer(ReducerFunction,initialstate)
+// };
+
+// export default UseReducerExample;
